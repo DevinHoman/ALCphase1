4967cafaacf4fb8fdd2f518fa22d033cafe1baf2
Devin <devinhoman93@gmail.com>
2019-07-13 11:08:34 +0200

    13-07-2019
    Webview working
    
    Home screen created
    
    Intents created for btn

--- app/src/main/java/com/example/alcphase1/HomeScreenActivity.java
+++ app/src/main/java/com/example/alcphase1/HomeScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.alcphase1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -19,7 +21,22 @@ public class HomeScreenActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Button aboutBtn = findViewById(R.id.btn_home_aboutalc);
+        Button myProfileBtn = findViewById(R.id.btn_home_myprofile);
 
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreenActivity.this,WebActivity.class));
+            }
+        });
+
+        myProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeScreenActivity.this,MyProfileActivity.class));
+            }
+        });
     }
 
 }
