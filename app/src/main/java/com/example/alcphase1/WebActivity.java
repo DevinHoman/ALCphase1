2b13cccde369c776e9294f06f145740d340fe85c
Devin <devinhoman93@gmail.com>
2019-07-13 10:53:54 +0200

    13-07-2019
    Webview working
    
    Home screen created

--- app/src/main/java/com/example/alcphase1/WebActivity.java
+++ app/src/main/java/com/example/alcphase1/WebActivity.java
@@ -1,23 +1,19 @@
 package com.example.alcphase1;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.ProgressDialog;
+
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.webkit.WebResourceRequest;
+
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+
 
 public class WebActivity extends AppCompatActivity {
 
